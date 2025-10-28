7fe3a65f3f7cb25dd7978c65b195729603d9175a
Maarten Majoor <majoor@MacBook-Pro-van-Maarten.local>
2025-10-28 11:31:23 +0100

    add functions to nb

--- src/constants/config.py
+++ src/constants/config.py
@@ -1,3 +1,4 @@
 path_transactions = r"data/raw/Transactions.csv"
 path_account = r"data/raw/Account.csv"
-path_portfolio = "data/processed/daily_portfolio.csv"
+path_portfolio = r"data/processed/daily_portfolio.csv"
+path_db = r"data/db"
